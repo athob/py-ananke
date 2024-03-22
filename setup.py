3ddf12b68315030c3812fbcdef92cce92af1a226
Adrien Thob <adrien.thob@gmail.com>
2024-03-22 12:28:59 -0400

    Aligning python version requirement with numpy minimal requirement

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(name=NAME,
           "Topic :: Scientific/Engineering :: Astronomy",
           "Topic :: Software Development :: Version Control :: Git"
       ],
-      python_requires='>=3.7.12',
+      python_requires='>=3.8',
       packages=[NAME],
       package_dir={'': SRC_DIR},
       install_requires=["numpy>=1.22,<2", "scipy>=1.7.2,<2", "pandas>=2,<3",
