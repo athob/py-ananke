a071902c86a8937f279e57476546962c0e4b62e7
Adrien Thob <adrien.thob@gmail.com>
2024-08-28 15:43:11 -0400

    More formatting improvements & temporarily adjusting nb_st timeout parameter

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,7 +42,7 @@ exclude_patterns = ['.ipynb_checkpoints/*']
 
 napoleon_use_param = False
 
-nb_execution_timeout = 1000
+nb_execution_timeout = 100
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
