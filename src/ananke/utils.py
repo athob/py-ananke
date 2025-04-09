e7ef930ff636514c71348732fb41f7e405f3ca90
Adrien Thob <adrien.thob@gmail.com>
2025-04-09 15:17:45 -0400

    Tidying up file

--- src/ananke/utils.py
+++ src/ananke/utils.py
@@ -2,7 +2,7 @@
 """
 Module miscellaneous utilities
 """
-from typing import Optional, List, Union
+from typing import Optional, Tuple, List, Union
 from numpy.typing import NDArray
 import re
 import docstring_parser as DS_parser
@@ -110,72 +110,10 @@ class LinearNDInterpolatorLOSExtrapolator:
         - https://stackoverflow.com/a/30654855
         """
         self._spherical: bool = spherical
-        if not self._spherical:
-            center = points.shape[1]*[0.]
-            if not center in points.tolist():
-                points = np.vstack([center, points])
-                values = np.hstack([value_at_center, values])
-        else:
-            # if spherical, transform points coordinates to [radius, lon, lat]
-            points = self._convert_cartesian_points_to_spherical(points)
-            convex_hull = spatial.ConvexHull(points)
-            # find hull faces that are almost parallel & nearest r=0 plane
-            nearzero_faces = (np.abs(convex_hull.equations.T[0]) > np.linalg.norm(convex_hull.equations.T[[1,2]],axis=0)) & (convex_hull.equations.T[0] < 0)
-            # find corresponding faces centroids
-            nearzero_centroids = np.average(convex_hull.points[convex_hull.simplices[nearzero_faces]],axis=1)
-            nearzero_centroids = nearzero_centroids[nearzero_centroids[:,0]!=0]
-            # project them on r=0 plane
-            nearzero_centroids[:,0] = 0.
-            zero_convex_hull = spatial.ConvexHull(nearzero_centroids[:,1:])
-            # mask border edges that are near parallel to actual -+180,-+90 box
-            left_edges = (np.abs(zero_convex_hull.equations.T[0]) > np.abs(zero_convex_hull.equations.T[1])) & (zero_convex_hull.equations.T[0] < 0)
-            right_edges = (np.abs(zero_convex_hull.equations.T[0]) > np.abs(zero_convex_hull.equations.T[1])) & (zero_convex_hull.equations.T[0] > 0)
-            bottom_edges = (np.abs(zero_convex_hull.equations.T[0]) < np.abs(zero_convex_hull.equations.T[1])) & (zero_convex_hull.equations.T[1] < 0)
-            top_edges = (np.abs(zero_convex_hull.equations.T[0]) < np.abs(zero_convex_hull.equations.T[1])) & (zero_convex_hull.equations.T[1] > 0)
-            # find corresponding edge centroids
-            left_edge_centroids = np.average(zero_convex_hull.points[zero_convex_hull.simplices[left_edges]],axis=1)
-            right_edge_centroids = np.average(zero_convex_hull.points[zero_convex_hull.simplices[right_edges]],axis=1)
-            bottom_edge_centroids = np.average(zero_convex_hull.points[zero_convex_hull.simplices[bottom_edges]],axis=1)
-            top_edge_centroids = np.average(zero_convex_hull.points[zero_convex_hull.simplices[top_edges]],axis=1)
-            left_edge_centroids = left_edge_centroids[left_edge_centroids[:,0]!=-180]
-            right_edge_centroids = right_edge_centroids[right_edge_centroids[:,0]!=180]
-            bottom_edge_centroids = bottom_edge_centroids[bottom_edge_centroids[:,1]!=-90]
-            top_edge_centroids = top_edge_centroids[top_edge_centroids[:,1]!=90]
-            # project them onto corresponding box edge
-            left_edge_centroids[:,0] = -180
-            right_edge_centroids[:,0] = 180
-            bottom_edge_centroids[:,1] = -90
-            top_edge_centroids[:,1] = 90
-            # make sure no latitude is duplicated on -+180 longitude edge
-            left_edge_centroids = left_edge_centroids[~np.in1d(left_edge_centroids[:,1], right_edge_centroids[:,1])]
-            right_edge_centroids = right_edge_centroids[~np.in1d(right_edge_centroids[:,1], left_edge_centroids[:,1])]
-            # establish new corresponding border points
-            border_centroids = np.vstack([left_edge_centroids, bottom_edge_centroids, top_edge_centroids, right_edge_centroids])
-            border_centroids = np.hstack([np.zeros(border_centroids.shape[0])[:,np.newaxis], border_centroids])
-            # establish complete set of points on r=0 plane, and append points and values accordingly
-            zeros_centroids = np.vstack([nearzero_centroids, border_centroids])
-            points = np.vstack([zeros_centroids, points])
-            values = np.hstack([value_at_center*np.ones(zeros_centroids.shape[0]), values])
-            # extend & duplicate points beyond -+180 towards -+360 in longitude
-            points_left_mask = points[:,1] <= 0
-            points_right_mask = points[:,1] >= 0
-            points = np.vstack([points[points_right_mask]-[0,360,0], points, points[points_left_mask]+[0,360,0]])
-            values = np.hstack([values[points_right_mask], values, values[points_left_mask]])
-            # add corner points if those are missing
-            temp = points.tolist()
-            corners = np.array([foo for l in [-180.,180.] for b in [-90.,90.] if not (foo:=[0.,l,b]) in temp])
-            points = np.vstack([corners, points])
-            values = np.hstack([value_at_center*np.ones(corners.shape[0]), values])
-        #
+        points, values = self._complete_points_values_with_center(points, values, value_at_center, self._spherical)
         self.linear_interpolator = interpolate.LinearNDInterpolator(points, values, **kwargs)
         self._calibrating_center = np.mean(points,axis=0)
         self.linear_interpolator(self._calibrating_center)
-        # from_calibrating_center = points - self._calibrating_center
-        # self._calibrating_outer = self._calibrating_center + 2*from_calibrating_center[
-        #     np.argmax(np.linalg.norm(from_calibrating_center)
-        #               if points.ndim == 2
-        #               else np.abs(from_calibrating_center))
-        #               ]
         self._convex_hull = spatial.ConvexHull(self.linear_interpolator.tri.points[np.unique(self.linear_interpolator.tri.convex_hull)])
         self._hull_equations = self._convex_hull.equations.T
         self._hull_normals, self._hull_offsets = self._hull_equations[:-1], self._hull_equations[-1]
@@ -230,6 +168,68 @@ class LinearNDInterpolatorLOSExtrapolator:
             return t.item(0)
         return t
     
+    @classmethod
+    def _complete_points_values_with_center(cls, points: NDArray, values: NDArray,
+                                                 value_at_center: float,
+                                                 spherical: bool) -> Tuple[NDArray,NDArray]:
+        if not spherical:
+            center = points.shape[1]*[0.]
+            if not center in points.tolist():
+                points = np.vstack([center, points])
+                values = np.hstack([value_at_center, values])
+        else:
+            # if spherical, transform points coordinates to [radius, lon, lat]
+            points = cls._convert_cartesian_points_to_spherical(points)
+            convex_hull = spatial.ConvexHull(points)
+            # find hull faces that are almost parallel & nearest r=0 plane
+            nearzero_faces = (np.abs(convex_hull.equations.T[0]) > np.linalg.norm(convex_hull.equations.T[[1,2]],axis=0)) & (convex_hull.equations.T[0] < 0)
+            # find corresponding faces centroids
+            nearzero_centroids = np.average(convex_hull.points[convex_hull.simplices[nearzero_faces]],axis=1)
+            nearzero_centroids = nearzero_centroids[nearzero_centroids[:,0]!=0]
+            # project them on r=0 plane
+            nearzero_centroids[:,0] = 0.
+            zero_convex_hull = spatial.ConvexHull(nearzero_centroids[:,1:])
+            # mask border edges that are near parallel to actual -+180,-+90 box
+            left_edges = (np.abs(zero_convex_hull.equations.T[0]) > np.abs(zero_convex_hull.equations.T[1])) & (zero_convex_hull.equations.T[0] < 0)
+            right_edges = (np.abs(zero_convex_hull.equations.T[0]) > np.abs(zero_convex_hull.equations.T[1])) & (zero_convex_hull.equations.T[0] > 0)
+            bottom_edges = (np.abs(zero_convex_hull.equations.T[0]) < np.abs(zero_convex_hull.equations.T[1])) & (zero_convex_hull.equations.T[1] < 0)
+            top_edges = (np.abs(zero_convex_hull.equations.T[0]) < np.abs(zero_convex_hull.equations.T[1])) & (zero_convex_hull.equations.T[1] > 0)
+            # find corresponding edge centroids
+            left_edge_centroids = np.average(zero_convex_hull.points[zero_convex_hull.simplices[left_edges]],axis=1)
+            right_edge_centroids = np.average(zero_convex_hull.points[zero_convex_hull.simplices[right_edges]],axis=1)
+            bottom_edge_centroids = np.average(zero_convex_hull.points[zero_convex_hull.simplices[bottom_edges]],axis=1)
+            top_edge_centroids = np.average(zero_convex_hull.points[zero_convex_hull.simplices[top_edges]],axis=1)
+            left_edge_centroids = left_edge_centroids[left_edge_centroids[:,0]!=-180]
+            right_edge_centroids = right_edge_centroids[right_edge_centroids[:,0]!=180]
+            bottom_edge_centroids = bottom_edge_centroids[bottom_edge_centroids[:,1]!=-90]
+            top_edge_centroids = top_edge_centroids[top_edge_centroids[:,1]!=90]
+            # project them onto corresponding box edge
+            left_edge_centroids[:,0] = -180
+            right_edge_centroids[:,0] = 180
+            bottom_edge_centroids[:,1] = -90
+            top_edge_centroids[:,1] = 90
+            # make sure no latitude is duplicated on -+180 longitude edge
+            left_edge_centroids = left_edge_centroids[~np.in1d(left_edge_centroids[:,1], right_edge_centroids[:,1])]
+            right_edge_centroids = right_edge_centroids[~np.in1d(right_edge_centroids[:,1], left_edge_centroids[:,1])]
+            # establish new corresponding border points
+            border_centroids = np.vstack([left_edge_centroids, bottom_edge_centroids, top_edge_centroids, right_edge_centroids])
+            border_centroids = np.hstack([np.zeros(border_centroids.shape[0])[:,np.newaxis], border_centroids])
+            # establish complete set of points on r=0 plane, and append points and values accordingly
+            zeros_centroids = np.vstack([nearzero_centroids, border_centroids])
+            points = np.vstack([zeros_centroids, points])
+            values = np.hstack([value_at_center*np.ones(zeros_centroids.shape[0]), values])
+            # extend & duplicate points beyond -+180 towards -+360 in longitude
+            points_left_mask = points[:,1] <= 0
+            points_right_mask = points[:,1] >= 0
+            points = np.vstack([points[points_right_mask]-[0,360,0], points, points[points_left_mask]+[0,360,0]])
+            values = np.hstack([values[points_right_mask], values, values[points_left_mask]])
+            # add corner points if those are missing
+            temp = points.tolist()
+            corners = np.array([foo for l in [-180.,180.] for b in [-90.,90.] if not (foo:=[0.,l,b]) in temp])
+            points = np.vstack([corners, points])
+            values = np.hstack([value_at_center*np.ones(corners.shape[0]), values])
+        return points, values
+
     @staticmethod
     def _convert_cartesian_points_to_spherical(points: NDArray) -> NDArray:
         r,lat,lon = coordinates.cartesian_to_spherical(*tuple(points.T))
