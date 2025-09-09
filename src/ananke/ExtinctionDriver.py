40d8ae54b6bd8a3dd040bf03682b433f4a66459f
Adrien Thob <adrien.thob@gmail.com>
2025-09-08 22:48:32 -0400

    Last change to the header notices

--- src/ananke/ExtinctionDriver.py
+++ src/ananke/ExtinctionDriver.py
@@ -3,8 +3,9 @@
 # Author: Adrien CR Thob
 # Copyright (C) 2022  Adrien CR Thob
 #
-# This file is part of py-ananke:
-# <https://github.com/athob/py-ananke>.
+# This file is part of the py-ananke project,
+# <https://github.com/athob/py-ananke>, which is licensed
+# under the GNU Affero General Public License v3.0 (AGPL-3.0).
 # 
 # The full copyright notice, including terms governing use, modification,
 # and redistribution, is contained in the files LICENSE and COPYRIGHT,
